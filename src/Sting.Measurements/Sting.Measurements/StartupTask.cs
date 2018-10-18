bb3d21d249a8f10ace668a44ef33022ff8ec9d67
Robin_tty <muellerobin95@gmail.com>
2018-10-18 02:17:40 +0200

    Implement Timer

--- src/Sting.Measurements/Sting.Measurements/StartupTask.cs
+++ src/Sting.Measurements/Sting.Measurements/StartupTask.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Http;
+using System.Threading;
 using Windows.Devices.Gpio;
+using Sensors.Dht;
 using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Automation;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -12,9 +15,31 @@ namespace Sting.Measurements
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        
+        private const int DhtPin = 4;
+        private GpioPin _winDhtPin = null;
+        private IDht _dht = null;
+        
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            
+            // Open a connection to the used GPIO pin 4 in exclusive mode
+            _winDhtPin = GpioController.GetDefault().OpenPin(DhtPin, GpioSharingMode.Exclusive);
+            _dht = new Dht11(_winDhtPin, GpioPinDriveMode.Input);
+            InitTimer();
+            for (;;);
+        }
+
+        private static void InitTimer()
+        {
+            //implement timer to perform a measurement every x seconds
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(2);
+            var stateTimer = new Timer((e) => { TakeMeasurement(); }, null, startTimeSpan, periodTimeSpan);
+        }
+
+        private static void TakeMeasurement()
+        {
+
         }
     }
 }
