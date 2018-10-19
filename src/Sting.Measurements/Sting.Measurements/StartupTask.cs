be8bb4139cb5a3fa3b233dd748f521ce25bc006f
Robin_tty <muellerobin95@gmail.com>
2018-10-19 15:27:57 +0200

    Add functionality to take measurement

--- src/Sting.Measurements/Sting.Measurements/StartupTask.cs
+++ src/Sting.Measurements/Sting.Measurements/StartupTask.cs
@@ -15,31 +15,43 @@ namespace Sting.Measurements
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        
-        private const int DhtPin = 4;
-        private GpioPin _winDhtPin = null;
-        private IDht _dht = null;
+        private static IDht _dht = null;
         
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // Open a connection to the used GPIO pin 4 in exclusive mode
-            _winDhtPin = GpioController.GetDefault().OpenPin(DhtPin, GpioSharingMode.Exclusive);
-            _dht = new Dht11(_winDhtPin, GpioPinDriveMode.Input);
-            InitTimer();
+            InitGpio();
+            InitTimedMeasurement(20);
             for (;;);
         }
 
-        private static void InitTimer()
+        private static void InitGpio()
         {
-            //implement timer to perform a measurement every x seconds
-            var startTimeSpan = TimeSpan.Zero;
-            var periodTimeSpan = TimeSpan.FromSeconds(2);
-            var stateTimer = new Timer((e) => { TakeMeasurement(); }, null, startTimeSpan, periodTimeSpan);
+            // Open a connection to the used GPIO pin 4 in exclusive mode
+            const int dhtPin = 4;
+            GpioPin winDhtPin = null;
+            winDhtPin = GpioController.GetDefault().OpenPin(dhtPin, GpioSharingMode.Exclusive);
+            _dht = new Dht11(winDhtPin, GpioPinDriveMode.Input);
         }
 
-        private static void TakeMeasurement()
+        private static void InitTimedMeasurement(int period)
         {
+            // Implement timer to perform a measurement every x seconds
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(period);
+            var stateTimer = new Timer(TakeMeasurement, null, startTimeSpan, periodTimeSpan);
+        }
 
+        private static async void TakeMeasurement(object state)
+        {
+            // Take measurement and check for validity
+            double temp = 0.0;
+            double humidity = 0.0;
+            DhtReading measurement = await _dht.GetReadingAsync().AsTask();
+            if (measurement.IsValid)
+            {
+                temp = measurement.Temperature;
+                humidity = measurement.Humidity;
+            }
         }
     }
 }
