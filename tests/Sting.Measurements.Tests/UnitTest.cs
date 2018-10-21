51d8d9508bed9f8d29d27ca3156feb09dcec8510
Robin_tty <muellerobin95@gmail.com>
2018-10-21 22:25:18 +0200

    Add test project

---
+++ tests/Sting.Measurements.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sting.Measurements.Tests
+{
+    [TestClass]
+    public class TelemetryDataTest
+    {
+        [TestMethod]
+        public void TelemetryObjectToJson()
+        {
+            var telemetry = new TelemetryData {Temperature = 20.2, Humidity = 57, Timestamp = DateTime.Now};
+        }
+    }
+}
