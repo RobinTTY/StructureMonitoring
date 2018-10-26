33a42f9da3a7e4bce07731d21d23537103751010
Robin_tty <muellerobin95@gmail.com>
2018-10-26 16:02:48 +0200

    Add State() method to Sensor interface + documentation

--- src/Sting.Measurements/Sting.Measurements/ISensor.cs
+++ src/Sting.Measurements/Sting.Measurements/ISensor.cs
@@ -8,6 +8,17 @@ namespace Sting.Measurements
 {
     interface ISensor
     {
+        /// <summary>
+        /// Initiates the sensor component, making it ready for use.
+        /// </summary>
+        /// <param name="pin">The data pin the sensor component uses.</param>
+        /// <returns>Returns True if initiation was successful.</returns>
         bool InitSensor(int pin);
+
+        /// <summary>
+        /// Returns the state of the sensor component.
+        /// </summary>
+        /// <returns>Returns True if component is on.</returns>
+        bool State();
     }
 }
