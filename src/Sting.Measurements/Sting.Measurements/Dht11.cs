736015b1af452338bc3b99b6e9dbc75cd7ee3d64
Robin_tty <muellerobin95@gmail.com>
2018-10-26 16:07:28 +0200

    Add Dht11 class

---
+++ src/Sting.Measurements/Sting.Measurements/Dht11.cs
@@ -0,0 +1,25 @@
+﻿using Windows.Devices.Gpio;
+using Sensors.Dht;
+
+namespace Sting.Measurements
+{
+    class Dht11 : ISensor
+    {
+        private GpioPin _pin;
+
+        public Dht11(int pin)
+        {
+            InitSensor(pin);
+        }
+
+        public bool InitSensor(int pin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool State()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
