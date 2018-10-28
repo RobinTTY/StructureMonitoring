d34500286a651d3334a6a4875fa3431e146d4aa4
Robin_tty <muellerobin95@gmail.com>
2018-10-28 21:52:53 +0100

    Overload ToString method and add constructor

--- src/Sting.Measurements/Sting.Measurements/TelemetryData.cs
+++ src/Sting.Measurements/Sting.Measurements/TelemetryData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Newtonsoft.Json;
 
 namespace Sting.Measurements
@@ -9,9 +10,21 @@ namespace Sting.Measurements
         public double Temperature;
         public double Humidity;
 
+        public TelemetryData()
+        {
+            Timestamp = DateTime.Now;
+            Temperature = 0.0;
+            Humidity = 0.0;
+        }
+
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this);
+        }
+
+        public override string ToString()
+        {
+            return "Time: " + Timestamp.ToString(CultureInfo.InvariantCulture) + ", Temperature: " + Temperature.ToString(CultureInfo.InvariantCulture) + "°C, Humidity: " + Humidity.ToString(CultureInfo.InvariantCulture) + "%";
         }
     }
 }
