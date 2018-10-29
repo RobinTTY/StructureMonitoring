1a150f37817d1aab344b72e61bd73cab8ce2234b
Robin_tty <muellerobin95@gmail.com>
2018-10-29 22:39:11 +0100

    Add AzureIotHub class and DeviceToCloud Method

---
+++ src/Sting.Measurements/Sting.Cloud/AzureIoTHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace Sting.Cloud
+{
+    public class AzureIotHub
+    {
+        private const string ConnectionString = "{connection string}";
+
+        /// <summary>
+        /// Sends a given string to the IoT Hub which the connection string points to.
+        /// </summary>
+        /// <param name="msg">A String of variable length.</param>
+        public static async Task SendDeviceToCloudMessage(string msg)
+        {
+            
+            var deviceClient = DeviceClient.CreateFromConnectionString(ConnectionString, TransportType.Mqtt);
+            var message = new Message(Encoding.ASCII.GetBytes(msg));
+
+            await deviceClient.SendEventAsync(message);
+        }
+    }
+}
