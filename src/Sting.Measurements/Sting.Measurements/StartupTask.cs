d34bcdbcb645ef7b009544006d2ae5244508b459
Robin_tty <muellerobin95@gmail.com>
2018-10-29 22:41:12 +0100

    Send telemetry data to Azure IotHub

--- src/Sting.Measurements/Sting.Measurements/StartupTask.cs
+++ src/Sting.Measurements/Sting.Measurements/StartupTask.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using Windows.ApplicationModel.Background;
 using Windows.System.Threading;
+using Sting.Cloud;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -18,16 +20,21 @@ namespace Sting.Measurements
             _deferral = taskInstance.GetDeferral();
             _tempSensor.InitSensor(4);
             _statusLed.InitSensor(5);
-            ThreadPoolTimer timer = ThreadPoolTimer.CreatePeriodicTimer(TakeMeasurement, TimeSpan.FromSeconds(2));
+            ThreadPoolTimer.CreatePeriodicTimer(TakeMeasurement, TimeSpan.FromSeconds(4));
         }
 
-        private void TakeMeasurement(ThreadPoolTimer timer)
+        private async void TakeMeasurement(ThreadPoolTimer timer)
         {
             if (_cancelRequested == false)
             {
                 var telemetry = _tempSensor.TakeMeasurement();
                 if (telemetry.Result == null) _statusLed.TurnOff();
-                else _statusLed.TurnOn();
+                else
+                {
+                    _statusLed.TurnOn();
+                    await AzureIotHub.SendDeviceToCloudMessage(telemetry.Result.ToJson());
+                    Debug.WriteLine("Message sent!");
+                }
             }
             else
             {
