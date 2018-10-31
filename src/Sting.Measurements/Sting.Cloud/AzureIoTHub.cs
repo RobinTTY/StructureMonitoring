2c0845aaf0e603c899eff4ed0d7e3849c717eafe
Robin_tty <muellerobin95@gmail.com>
2018-10-31 23:11:45 +0100

    Add constructor, remove redundant exception handling

--- src/Sting.Measurements/Sting.Cloud/AzureIoTHub.cs
+++ src/Sting.Measurements/Sting.Cloud/AzureIoTHub.cs
@@ -10,8 +10,18 @@ namespace Sting.Cloud
 {
     public class AzureIotHub
     {
-        private readonly string _connectionString = System.IO.File.ReadAllText(@"C:\Data\Users\DefaultAccount\AppData\Local\Packages\Sting.Measurements-uwp_gk6cf97c3a7py\LocalState\DeviceConnectionString.txt");
-        
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// If given string is a file path of an existing file, the contents of the file will be used as connection string.
+        /// If given string is not the file path to an existing file, the string will be set as connection string itself.
+        /// </summary>
+        /// <param name="connectionString">File path or connection string.</param>
+        public AzureIotHub(string connectionString = "C:\\Data\\Users\\DefaultAccount\\AppData\\Local\\Packages\\Sting.Measurements-uwp_gk6cf97c3a7py\\LocalState\\DeviceConnectionString.txt")
+        {
+            _connectionString = System.IO.File.Exists(connectionString) ? System.IO.File.ReadAllText(connectionString) : connectionString;
+        }
+
         /// <summary>
         /// Sends a given string to the IoT Hub which the connection string points to.
         /// </summary>
@@ -20,17 +30,8 @@ namespace Sting.Cloud
         {
             var deviceClient = DeviceClient.CreateFromConnectionString(_connectionString, TransportType.Mqtt);
             var message = new Message(Encoding.ASCII.GetBytes(msg));
-
-            try
-            {
-                await deviceClient.SendEventAsync(message);
-            }
-            catch (ClosedChannelException e)
-            {
-                Debug.WriteLine(e.StackTrace);
-                Debug.WriteLine("Message: " + e.Message);
-                Debug.WriteLine("Source: " + e.Source);
-            }
+            
+            await deviceClient.SendEventAsync(message);
             deviceClient.Dispose();
         }
     }
