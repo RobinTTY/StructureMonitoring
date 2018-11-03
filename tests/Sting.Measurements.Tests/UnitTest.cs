90bc2d045935459a899f8c28e987ea67cd13c48f
Robin_tty <muellerobin95@gmail.com>
2018-11-03 19:49:20 +0100

    Update Unit Test

--- tests/Sting.Measurements.Tests/UnitTest.cs
+++ tests/Sting.Measurements.Tests/UnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace Sting.Measurements.Tests
 {
@@ -10,20 +11,22 @@ namespace Sting.Measurements.Tests
         [TestMethod]
         public void TelemetryObjectToJson()
         {
-            var telemetry = new TelemetryData {Temperature = 20.2, Humidity = 57, Timestamp = DateTime.Now};
-            Assert.IsNotNull(telemetry.ToJson());
+            var telemetry = new TelemetryData { Temperature = 20.2, Humidity = 57, Timestamp = DateTime.Now };
+            var conversionResult = JsonConvert.SerializeObject(telemetry);
+            Assert.AreEqual(telemetry.ToJson(), conversionResult);
         }
     }
 
     [TestClass]
     public class LedTest
     {
-        private static readonly Led Led = new Led(5);
+        private static readonly Led Led = new Led();
 
         [TestMethod]
         public void TestMethodOn()
         {
             // turn Led on and wait for Hardware
+            Led.InitSensor(5);
             Led.TurnOff();
             Led.TurnOn();
             Task.Delay(3 * 1000).Wait();
