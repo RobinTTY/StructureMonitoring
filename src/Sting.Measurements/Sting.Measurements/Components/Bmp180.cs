7aebeb2b46544c1d3c1b06e784ea4ac3e91e94c8
Robin_tty <muellerobin95@gmail.com>
2018-11-05 01:24:38 +0100

    Add Bmp180 class

---
+++ src/Sting.Measurements/Sting.Measurements/Components/Bmp180.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sting.Measurements.Components
+{
+    class Bmp180 : IGpioComponent
+    {
+        public bool InitSensor(int pin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool State()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
