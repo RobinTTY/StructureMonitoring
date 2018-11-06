a3f2b0cb25ce8c7a2f674e6045964b9333ed9613
Robin_tty <muellerobin95@gmail.com>
2018-11-06 22:13:36 +0100

    Add test for overwrite method

--- tests/Sting.Measurements.Tests/UnitTest.cs
+++ tests/Sting.Measurements.Tests/UnitTest.cs
@@ -30,6 +30,20 @@ namespace Sting.Measurements.Tests
             Assert.AreEqual(object1.Pressure, objectResult.Pressure);
             Assert.AreEqual(object1.Altitude, objectResult.Altitude);
         }
+
+        [TestMethod]
+        public void Overwrite()
+        {
+            var object1 = new TelemetryData { Temperature = 19.4, Altitude = 320.4 };
+            var object2 = new TelemetryData { Altitude = 100, Pressure = 5000 };
+            var objectResult = new TelemetryData { Temperature = 19.4, Altitude = 100, Pressure = 5000 };
+
+            object1.Overwrite(object2);
+            Assert.AreEqual(object1.Temperature, objectResult.Temperature);
+            Assert.AreEqual(object1.Humidity, objectResult.Humidity);
+            Assert.AreEqual(object1.Pressure, objectResult.Pressure);
+            Assert.AreEqual(object1.Altitude, objectResult.Altitude);
+        }
     }
 
     [TestClass]
@@ -38,10 +52,10 @@ namespace Sting.Measurements.Tests
         private static readonly Led Led = new Led();
 
         [TestMethod]
-        public async void TestMethodOn()
+        public void TestMethodOn()
         {
             // turn Led on and wait for Hardware
-            await Led.InitComponentAsync(5);
+            InitComponent();
             Led.TurnOff();
             Led.TurnOn();
             Task.Delay(3 * 1000).Wait();
@@ -50,15 +64,20 @@ namespace Sting.Measurements.Tests
         }
 
         [TestMethod]
-        public async void TestMethodOff()
+        public void TestMethodOff()
         {
             // turn Led off and wait for Hardware
-            await Led.InitComponentAsync(5);
+            InitComponent();
             Led.TurnOn();
             Led.TurnOff();
             Task.Delay(3 * 1000).Wait();
             var state = Led.State();
             Assert.IsFalse(state);
+        }
+
+        public async void InitComponent()
+        {
+            await Led.InitComponentAsync(5);
         }
     }
 }
