67a137b628e498d63655c55888c0d856c8591087
Robin_tty <muellerobin95@gmail.com>
2018-11-06 20:25:40 +0100

    Add test for Complement method

--- tests/Sting.Measurements.Tests/UnitTest.cs
+++ tests/Sting.Measurements.Tests/UnitTest.cs
@@ -10,11 +10,25 @@ namespace Sting.Measurements.Tests
     public class TelemetryDataTest
     {
         [TestMethod]
-        public void TelemetryObjectToJson()
+        public void ToJson()
         {
             var telemetry = new TelemetryData { Temperature = 20.2, Humidity = 57, Timestamp = DateTime.Now };
             var conversionResult = JsonConvert.SerializeObject(telemetry);
             Assert.AreEqual(telemetry.ToJson(), conversionResult);
+        }
+
+        [TestMethod]
+        public void Complement()
+        {
+            var object1 = new TelemetryData {Temperature = 19.4, Altitude = 320.4};
+            var object2 = new TelemetryData {Altitude = 100, Pressure = 5000};
+            var objectResult = new TelemetryData {Temperature = 19.4, Altitude = 320.4, Pressure = 5000};
+
+            object1.Complement(object2);
+            Assert.AreEqual(object1.Temperature, objectResult.Temperature);
+            Assert.AreEqual(object1.Humidity, objectResult.Humidity);
+            Assert.AreEqual(object1.Pressure, objectResult.Pressure);
+            Assert.AreEqual(object1.Altitude, objectResult.Altitude);
         }
     }
 
