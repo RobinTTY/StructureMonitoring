fe0d421a83cf7bcb8f6f86948b5f2b8f26c0514c
Robin_tty <muellerobin95@gmail.com>
2018-11-15 19:23:58 +0100

    Cleanup

--- src/Sting.Measurements/Sting.Measurements/IGpioComponent.cs
+++ src/Sting.Measurements/Sting.Measurements/IGpioComponent.cs
@@ -11,15 +11,16 @@ namespace Sting.Measurements
         /// If the component uses the I2C bus, the pin parameter is not required.</param>
         /// <returns>Returns True if initiation was successful.</returns>
         Task<bool> InitComponentAsync(int pin);
+        // TODO: Throw Exception if component gets initialized twice
 
         /// <summary>
-        /// Returns the state of the sensor component.
+        /// Returns the state of the gpio component.
         /// </summary>
         /// <returns>Returns True if component is initiated.</returns>
         bool State();
 
         /// <summary>
-        /// Closes the Pin associated with the Component, making it available for other devices.
+        /// Closes the pin associated with the component, making it available for other devices.
         /// </summary>
         void ClosePin();
     }
--- src/Sting.Measurements/Sting.Measurements/TelemetryData.cs
+++ src/Sting.Measurements/Sting.Measurements/TelemetryData.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Reflection;
 using Newtonsoft.Json;
 using Sensors.Dht;
-using BMP;
+using Sting.Measurements.External_Libraries;
 
 namespace Sting.Measurements
 {
