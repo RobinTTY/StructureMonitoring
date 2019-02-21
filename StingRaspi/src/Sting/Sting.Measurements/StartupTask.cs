27e48bde14e8b5893bc76114a6293495e0370267
Robin_tty <muellerobin95@gmail.com>
2019-02-21 23:45:38 +0100

    Test Database connection

--- StingRaspi/src/Sting/Sting.Measurements/StartupTask.cs
+++ StingRaspi/src/Sting/Sting.Measurements/StartupTask.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Windows.ApplicationModel.Background;
 using Windows.System.Threading;
 using Sting.Measurements.Components;
+using Sting.Storage;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,79 +12,27 @@ namespace Sting.Measurements
     public sealed class StartupTask : IBackgroundTask
     {
         private BackgroundTaskDeferral _deferral;
-        private readonly Bmp180 _pressureSensor = new Bmp180();
-        private readonly Dht11 _tempSensor = new Dht11();
-        private readonly Buzzer _buzzer = new Buzzer();
-        private readonly Led _statusLed = new Led();
-        private readonly Lcd _lcd = new Lcd();
         private bool _cancelRequested;
+        private Database _stingDatabase = new Database();
         
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            _deferral = taskInstance.GetDeferral();
-            InitComponentsAsync();
-            InitDirectMethodCallsAsync();
-            ThreadPoolTimer.CreatePeriodicTimer(PeriodicTask, TimeSpan.FromSeconds(60));
+            _stingDatabase.InitConnection();
+            //_deferral = taskInstance.GetDeferral();
+            //ThreadPoolTimer.CreatePeriodicTimer(PeriodicTask, TimeSpan.FromSeconds(60));
         }
 
         // initialize used components async
         private async void InitComponentsAsync()
         {
-            await _tempSensor.InitComponentAsync(4);
-            await _pressureSensor.InitComponentAsync();
-            await _buzzer.InitComponentAsync(18);
-            await _statusLed.InitComponentAsync(5);
-            await _lcd.InitComponentAsync();
-
-            //print welcome message to lcd
-            _lcd.Write("Welcome to Sting");
-            _lcd.SetCursorPosition(2);
-            _lcd.Write("Initializing...");
-        }
-
-        // Register Direct Methods and set event handlers
-        private async void InitDirectMethodCallsAsync()
-        {
-            // await _structureMonitoringHub.RegisterDirectMethodsAsync();
-            //_structureMonitoringHub.Locate += _buzzer.OnLocate;
-            //_structureMonitoringHub.Terminate += OnTerminate;
+            // TODO: init
         }
 
         // Task which is executed every x seconds as defined in Run()
         // Take Measurements periodically
         private async void PeriodicTask(ThreadPoolTimer timer)
         {
-            if (_cancelRequested == false)
-            {
-                var combinedData = new TelemetryData();
-                var dhtTelemetry = await _tempSensor.TakeMeasurementAsync();
-                var bmpTelemetry = await _pressureSensor.TakeMeasurementAsync();
-
-                _lcd.ClearScreen();
-                // Use bmp measurements (duplicates) over dht measurements because of higher accuracy
-                if (dhtTelemetry == null && bmpTelemetry == null)
-                    _lcd.Write("Invalid Measurement");
-                else
-                {
-                    combinedData.Overwrite(dhtTelemetry);
-                    combinedData.Overwrite(bmpTelemetry);
-                    _lcd.Write($"Temp:{combinedData.Temperature:F1}{(char)223}C");    //prints the temp with one decimal place and the degree symbol
-                    _lcd.SetCursorPosition(2);
-                    _lcd.Write($"Hum:{combinedData.Humidity}% Alt:{combinedData.Altitude:F0}m");
-                }
-                
-                //var success = await _structureMonitoringHub.SendDeviceToCloudMessageAsync(combinedData.ToJson());
-                //Debug.WriteLine(success ? "Message sent!" : "Could not send your message.");
-                _statusLed.Blink(5000);
-            }
-            else
-            {
-                _lcd.ClearScreen();
-                _lcd.Write("Shutting down."); _lcd.SetCursorPosition(2); _lcd.Write("Goodbye ");
-                // indicate that deferral is completed
-                timer.Cancel();
-                _deferral.Complete();
-            }
+            // TODO: clean procedure
         }
 
         private void OnTerminate(object source, EventArgs e)
--- StingRaspi/src/Sting/Sting.Storage/Database.cs
+++ StingRaspi/src/Sting/Sting.Storage/Database.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Sting.Storage
 {
     public class Database
     {
+        public void InitConnection()
+        {
+            var client = new MongoClient("mongodb+srv://Robin:QLo0iZM4SWklq6HkL6gW@stingmeasurements-2u2yu.mongodb.net/test?retryWrites=true");
+            var database = client.GetDatabase("StingMeasurements");
+            database.RunCommandAsync((Command<BsonDocument>) "{ping:1}" ).Wait();
+        }
     }
 }
