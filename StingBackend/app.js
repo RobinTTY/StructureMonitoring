f875f11473b91bd64679a0096626208658b5b867
Robin_tty <muellerobin95@gmail.com>
2019-03-06 23:51:14 +0100

    Remove unnecessary dependencies

--- StingBackend/app.js
+++ StingBackend/app.js
@@ -1,35 +1,29 @@
-﻿'use strict';
-var debug = require('debug');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+﻿"use strict";
+var debug = require("debug");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var routes = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
@@ -38,28 +32,31 @@ app.use(function (req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+if (app.get("env") === "development") {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.render("error",
+            {
+                message: err.message,
+                error: err
+            });
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    res.render("error",
+        {
+            message: err.message,
+            error: {}
+        });
+});
+
+app.set("port", process.env.PORT || 3000);
+
+var server = app.listen(app.get("port"),
+    function() {
+        debug("Express server listening on port " + server.address().port);
     });
-});
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-});
