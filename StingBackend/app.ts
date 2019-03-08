f8f9aa1c5084bdf3aa7bb79d340f3bb276cf83e5
Robin_tty <muellerobin95@gmail.com>
2019-03-08 23:03:03 +0100

    Remove unneeded files, adhere to tslint

--- StingBackend/app.ts
+++ StingBackend/app.ts
@@ -1,25 +1,25 @@
-﻿import debug = require('debug');
-import express = require('express');
-import path = require('path');
+﻿import debug from "debug";
+import express from "express";
+import path from "path";
 
-import routes from './routes/index';
-import users from './routes/user';
+import routes from "./routes/index";
+import users from "./routes/user";
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err['status'] = 404;
+    const err = new Error("Not Found");
+    err["status"] = 404;
     next(err);
 });
 
@@ -27,10 +27,10 @@ app.use(function (req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
     app.use((err: any, req, res, next) => {
-        res.status(err['status'] || 500);
-        res.render('error', {
+        res.status(err["status"] || 500);
+        res.render("error", {
             message: err.message,
             error: err
         });
@@ -41,14 +41,14 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use((err: any, req, res, next) => {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
         error: {}
     });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+const server = app.listen(app.get("port"), function () {
+    debug("Express server listening on port " + server.address().port);
 });
--- StingBackend/routes/user.ts
+++ StingBackend/routes/user.ts
@@ -1,10 +1,10 @@
 ﻿/*
  * GET users listing.
  */
-import express = require('express');
+import express = require("express");
 const router = express.Router();
 
-router.get('/', (req: express.Request, res: express.Response) => {
+router.get("/", (req: express.Request, res: express.Response) => {
     res.send("respond with a resource");
 });
 
