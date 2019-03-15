7820ea6e93e2709e0e9cbe24458e5b3ae57754ed
Robin_tty <muellerobin95@gmail.com>
2019-03-15 23:36:45 +0100

    simplify for now

--- StingBackend/src/Database/Models/TelemetryData.ts
+++ StingBackend/src/Database/Models/TelemetryData.ts
@@ -2,11 +2,6 @@ import { Document, Schema, Model, model } from "mongoose";
 import { ITelemetryData } from "../Interfaces/ITelemetryData";
 
 export interface ITelemetryDataModel extends ITelemetryData, Document {
-    ObjectId(): String;
-    Timestamp(): Number;
-    Temperature(): Number;
-    Humidity(): Number;
-    AirPressure(): Number;
 }
 
 export const TelemetryDataSchema: Schema = new Schema({
@@ -16,25 +11,5 @@ export const TelemetryDataSchema: Schema = new Schema({
     humidity: Number,
     airPressure: Number,
 });
-
-TelemetryDataSchema.methods.ObjectId = function(): string {
-    return (this.objectId);
-};
-
-TelemetryDataSchema.methods.Timestamp = function(): string {
-    return (this.timeStamp);
-};
-
-TelemetryDataSchema.methods.Temperature = function(): string {
-    return (this.temperature);
-};
-
-TelemetryDataSchema.methods.Humidity = function(): string {
-    return (this.humidity);
-};
-
-TelemetryDataSchema.methods.AirPressure = function(): string {
-    return (this.airPressure);
-};
 
 export const TelemetryData: Model<ITelemetryDataModel> = model<ITelemetryDataModel>("TelemetryData", TelemetryDataSchema);
