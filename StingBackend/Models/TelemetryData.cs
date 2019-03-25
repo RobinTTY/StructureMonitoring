747c61a932d12d1ea8a2085df6ad5ef30ae01669
Robin_tty <muellerobin95@gmail.com>
2019-03-25 23:46:03 +0100

    Add TelemetryData Model, update dependencies and project to .Net Core 2.2

---
+++ StingBackend/Models/TelemetryData.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StingBackend.Models
+{
+    public class TelemetryData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Timestamp")]
+        public long UnixTimeStamp { get; set; }
+
+        [BsonElement("Temperature")]
+        public double Temperature { get; set; }
+
+        [BsonElement("Humidity")]
+        public double Humidity { get; set; }
+
+        [BsonElement("Pressure")]
+        public double Pressure { get; set; }
+    }
+}
