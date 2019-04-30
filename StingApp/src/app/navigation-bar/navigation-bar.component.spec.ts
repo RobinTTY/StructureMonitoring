7e4970c86156ab3a3a1a3a5cbe554d0e5cb507c2
Robin_tty <muellerobin95@gmail.com>
2019-04-30 22:42:00 +0200

    Rename sidebar component

--- StingApp/src/app/navigation-bar/navigation-bar.component.spec.ts
+++ StingApp/src/app/navigation-bar/navigation-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidebarComponent } from './sidebar.component';
+import { NavigationBar } from './navigation-bar.component';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+  let component: NavigationBar;
+  let fixture: ComponentFixture<NavigationBar>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ NavigationBar ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
+    fixture = TestBed.createComponent(NavigationBar);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ StingApp/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.scss']
+})
+export class NavigationBar implements OnInit {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+  }
+}
