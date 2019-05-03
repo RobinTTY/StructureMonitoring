a14760a1ec82acb02d3528f1f8ae0f87b30aeda5
Robin_tty <muellerobin95@gmail.com>
2019-05-03 23:13:19 +0200

    Add IService interface

---
+++ StingRaspi/src/Sting/Sting.Devices.Contracts/IService.cs
@@ -0,0 +1,20 @@
+﻿namespace Sting.Devices.Contracts
+{
+    public interface IService
+    {
+        /// <summary>
+        /// Indicates whether the service is currently running.
+        /// </summary>
+        bool IsRunning { get; set; }
+
+        /// <summary>
+        /// Starts the service.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stops the service.
+        /// </summary>
+        void Stop();
+    }
+}
