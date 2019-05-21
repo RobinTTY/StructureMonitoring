0674dad7a29c9731a9b89fade665a0b88c403b02
Robin_tty <muellerobin95@gmail.com>
2019-05-21 23:52:55 +0200

    Add CpuMonitor

---
+++ src/Sting.Measurements/Sting.Devices/Contracts/ICpuMonitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sting.Devices.Contracts
+{
+    public interface ICpuMonitor : ISensorController
+    {
+        /// <summary>
+        /// Indicates whether a temperature sensor is available for the CPU.
+        /// </summary>
+        bool SensorAvailable { get; }
+    }
+}
---
+++ src/Sting.Measurements/Sting.Devices/CpuMonitor.cs
@@ -0,0 +1,25 @@
+﻿using Iot.Device.CpuTemperature;
+using Sting.Devices.Contracts;
+using Sting.Models;
+
+namespace Sting.Devices
+{
+    public class CpuMonitor : ICpuMonitor
+    {
+        public bool SensorAvailable => _monitor.IsAvailable;
+        private readonly CpuTemperature _monitor;
+
+        public CpuMonitor()
+        {
+            _monitor = new CpuTemperature();
+        }
+
+        public MeasurementContainer TakeMeasurement()
+        {
+            // TODO: new Measurement Container entirely or new concept?!
+            var temperature = _monitor.Temperature.Celsius;
+
+            return new MeasurementContainer(temperature);
+        }
+    }
+}
