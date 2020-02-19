07bb7ff469c91f77a6b3f73e05096fa1936a622c
Robin_tty <muellerobin95@gmail.com>
2020-02-19 05:40:36 +0100

    Add simple Logger interface and implementation

---
+++ src/Sting.Measurements/Sting.Core/DebugLineLogger.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using Sting.Core.Contracts;
+
+namespace Sting.Core
+{
+    public class DebugLineLogger : ILogger
+    {
+        public void Log(string message) => Debug.WriteLine(message);
+    }
+}
