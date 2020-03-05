b0986238fbfa00969c80f25567eabbaa1870b161
RobinTTY <muellerobin95@gmail.com>
2020-03-05 06:55:41 +0100

    Upgrade Backend to .Net Core 3.1

--- src/Sting.Backend/Sting.Backend/Startup.cs
+++ src/Sting.Backend/Sting.Backend/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Sting.Backend.Services;
 
 namespace Sting.Backend
@@ -22,7 +23,8 @@ namespace Sting.Backend
         {
             // service registration is necessary to support constructor injection in consuming classes
             services.AddScoped<TelemetryDataService>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             // Add Cors middleware
             services.AddCors(options =>
@@ -36,7 +38,7 @@ namespace Sting.Backend
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
