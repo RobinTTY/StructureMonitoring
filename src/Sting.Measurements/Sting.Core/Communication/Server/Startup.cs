73e25af34084d01622dc46c1f72a071a2a7d1192
RobinTTY <muellerobin95@gmail.com>
2020-03-26 22:58:43 +0100

    Add startup class for grpc server

---
+++ src/Sting.Measurements/Sting.Core/Communication/Server/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sting.Core.Communication.Server.Services;
+
+namespace Sting.Core.Communication.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<ConfiguratorService>();
+            });
+        }
+    }
+}
