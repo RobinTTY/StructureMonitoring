f049ceca504137c8bad7ba40b7be1ec7dfa9de35
Robin_tty <muellerobin95@gmail.com>
2020-03-27 22:37:17 +0100

    Add basic grpc server class

---
+++ src/Sting.Measurements/Sting.Core/Communication/Server/GrpcServer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Sting.Core.Contracts;
+
+namespace Sting.Core.Communication.Server
+{
+    public class GrpcServer : IService
+    {
+        public State State { get; set; }
+        private readonly IHost _server;
+
+        public GrpcServer()
+        {
+            _server = Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }).Build();
+        }
+
+        public void Start() => _server.StartAsync();
+
+        public void Stop() => _server.StopAsync();
+    }
+}
