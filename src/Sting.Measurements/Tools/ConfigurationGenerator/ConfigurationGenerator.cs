f466c00c2a79e9ba60446654232be2d779f27642
Robin_tty <muellerobin95@gmail.com>
2020-03-28 04:36:37 +0100

    Update configuratior to use grpc classes

--- src/Sting.Measurements/Tools/ConfigurationGenerator/ConfigurationGenerator.cs
+++ src/Sting.Measurements/Tools/ConfigurationGenerator/ConfigurationGenerator.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
 using Iot.Device.Bmxx80;
 using Iot.Device.Bmxx80.FilteringMode;
+using Sting.Core.Communication;
 using Sting.Devices.Configurations;
-using Sting.Models.Configurations;
 
 namespace ConfigurationGenerator
 {
@@ -12,66 +13,73 @@ namespace ConfigurationGenerator
         {
             return new SystemConfiguration
             {
-                Info = new ConfigInfo
+                Metadata = new ConfigMetadata
                 {
                     Version = "1.0.0"
                 },
-                Database = new ConfigDatabase()
+                DatabaseConfig = new DatabaseConfig
                 {
                     Type = "MongoDB",
-                    Attributes = new ConfigDbAttributes
+                    Attributes = new DatabaseAttributesConfig
                     {
                         Name = "Sting",
                         ConnectionString = "test"
                     }
-                },
-                Devices = new List<ConfigDevice>()
+                }
             };
         }
 
         public static void AddBme680Config(this SystemConfiguration config)
         {
-            config.Devices.Add(new ConfigDevice()
+            var bme680Config = new Bme680Configuration
+            {
+                FilteringMode = Bme680FilteringMode.C3,
+                GasConversionIsEnabled = true,
+                HeaterIsEnabled = true,
+                HeaterProfiles = new List<Bme680HeaterConfiguration>
+                {
+                    new Bme680HeaterConfiguration
+                    {
+                        HeaterProfile = Bme680HeaterProfile.Profile1,
+                        Duration = 150,
+                        TargetTemperature = 320
+                    }
+                },
+                HumiditySampling = Sampling.HighResolution,
+                PressureSampling = Sampling.HighResolution,
+                TemperatureSampling = Sampling.HighResolution,
+                ActiveProfile = Bme680HeaterProfile.Profile1,
+                I2CAddress = Bme680.SecondaryI2cAddress
+            };
+
+            var bme680DeviceConfig = new DeviceConfig
             {
                 Name = "Bme680",
-                Configuration = new Bme680Configuration()
-                {
-                    FilteringMode = Bme680FilteringMode.C3,
-                    GasConversionIsEnabled = true,
-                    HeaterIsEnabled = true,
-                    HeaterProfiles = new List<Bme680HeaterConfiguration>
-                    {
-                        new Bme680HeaterConfiguration()
-                        {
-                            HeaterProfile = Bme680HeaterProfile.Profile1,
-                            Duration = 150,
-                            TargetTemperature = 320
-                        }
-                    },
-                    HumiditySampling = Sampling.HighResolution,
-                    PressureSampling = Sampling.HighResolution,
-                    TemperatureSampling = Sampling.HighResolution,
-                    ActiveProfile = Bme680HeaterProfile.Profile1,
-                    I2CAddress = Bme680.SecondaryI2cAddress
-                }
-            });
+                Configuration = JsonSerializer.Serialize(bme680Config)
+            };
+
+            config.DeviceConfig.Add(bme680DeviceConfig);
         }
 
         public static void AddBme280Config(this SystemConfiguration config)
         {
-            config.Devices.Add(new ConfigDevice()
+            var bme280Config = new Bme280Configuration
+            {
+                FilterMode = Bmx280FilteringMode.X2,
+                HumiditySampling = Sampling.UltraHighResolution,
+                PressureSampling = Sampling.UltraHighResolution,
+                TemperatureSampling = Sampling.UltraHighResolution,
+                I2CAddress = Bmx280Base.DefaultI2cAddress,
+                StandbyTime = StandbyTime.Ms500
+            };
+
+            var bme280DeviceConfig = new DeviceConfig
             {
                 Name = "Bme280",
-                Configuration = new Bme280Configuration()
-                {
-                    FilterMode = Bmx280FilteringMode.X2,
-                    HumiditySampling = Sampling.UltraHighResolution,
-                    PressureSampling = Sampling.UltraHighResolution,
-                    TemperatureSampling = Sampling.UltraHighResolution,
-                    I2CAddress = Bmx280Base.DefaultI2cAddress,
-                    StandbyTime = StandbyTime.Ms500
-                }
-            });
+                Configuration = JsonSerializer.Serialize(bme280Config)
+            };
+
+            config.DeviceConfig.Add(bme280DeviceConfig);
         }
     }
 }
